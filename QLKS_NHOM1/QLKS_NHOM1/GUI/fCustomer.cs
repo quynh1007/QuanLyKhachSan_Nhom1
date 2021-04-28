bd64428af5933b76282137c310880f566cf999c0
= <=>
2021-04-28 20:53:07 +0700

    do du lieu vao dgv

--- QLKS_NHOM1/QLKS_NHOM1/GUI/fCustomer.cs
+++ QLKS_NHOM1/QLKS_NHOM1/GUI/fCustomer.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QLKS_NHOM1.DAO;
 
 namespace QLKS_NHOM1.GUI
 {
     public partial class fCustomer : Form
     {
+        BindingSource CustomerList = new BindingSource();
         public fCustomer()
         {
             InitializeComponent();
+            LoadData();
+            BindingDataToForm();
         }
+
+        private void BindingDataToForm()
+        {
+            txtHoTen.DataBindings.Add(new Binding("Text", dgvCustomer.DataSource, "NameCustomer", true, DataSourceUpdateMode.Never));
+            dtNgayDat.DataBindings.Add(new Binding("Text", dgvCustomer.DataSource, "DateTimeCustomer", true, DataSourceUpdateMode.Never));
+            cbGioiTinh.DataBindings.Add(new Binding("Text", dgvCustomer.DataSource, "GenderCustomer", true, DataSourceUpdateMode.Never));
+            txtDiaChi.DataBindings.Add(new Binding("Text", dgvCustomer.DataSource, "AddressCustomer", true, DataSourceUpdateMode.Never));
+            txtCCCD.DataBindings.Add(new Binding("Text", dgvCustomer.DataSource, "IdCardCustomer", true, DataSourceUpdateMode.Never));
+            txtSDT.DataBindings.Add(new Binding("Text", dgvCustomer.DataSource, "PhoneNumber", true, DataSourceUpdateMode.Never));
+        }
+
+        private void LoadData()
+        {
+            dgvCustomer.DataSource = CustomerList;
+            LoadListCustomer();
+            EditDataGridViewHeader();
+        }
+
+        private void EditDataGridViewHeader()
+        {
+            dgvCustomer.Columns["CustomerId"].HeaderText = "Mã Khách Hàng";
+            dgvCustomer.Columns["NameCustomer"].HeaderText = "Họ Tên";
+            dgvCustomer.Columns["DateTimeCustomer"].HeaderText = "Ngày Đặt";
+            dgvCustomer.Columns["GenderCustomer"].HeaderText = "Giới Tính";
+            dgvCustomer.Columns["AddressCustomer"].HeaderText = "Địa Chỉ";
+            dgvCustomer.Columns["IdCardCustomer"].HeaderText = "CCCD";
+            dgvCustomer.Columns["PhoneNumber"].HeaderText = "Số Điện Thoại";
+        }
+
+        private void LoadListCustomer()
+        {
+            CustomerList.DataSource = CustomerDAO.Instance.GetAll();
+        }
+
     }
 }
