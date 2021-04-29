647d84567bb03d804a94fde1ae5fbe25b8d2e04f
dat78787878 <d782000@gmail.com>
2021-04-29 21:52:12 +0700

    add RoomDAO

---
+++ QLKS_NHOM1/QLKS_NHOM1/DAO/RoomDAO.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLKS_NHOM1.DataAccessLayer;
+using QLKS_NHOM1.Models;
+
+namespace QLKS_NHOM1.DAO
+{
+    class RoomDAO
+    {
+        private static RoomDAO instance;
+
+        internal static RoomDAO Instance
+        {
+            get { if (instance == null) instance = new RoomDAO(); return instance; }
+            private set { instance = value; }
+        }
+        public List<Room> GetAll()
+        {
+            List<Room> list = new List<Room>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("SP_Room_GetAll");
+            foreach (DataRow item in data.Rows)
+            {
+                Room entry = new Room(item);
+                list.Add(entry);
+            }
+            return list;
+        }
+        public bool Insert(string NameRoom, int Status, int RoomTypeId)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("SP_Room_Insert @NameRoom , @Status , @RoomTypeId", new object[] { NameRoom, Status , RoomTypeId });
+            return result > 0;
+        }
+        public bool Update(int RoomId, string NameRoom, int Status, int RoomTypeId)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("SP_Room_Update @RoomId , @NameRoom , @Status , @RoomTypeId", new object[] { RoomId, NameRoom, Status, RoomTypeId});
+            return result > 0;
+        }
+
+        public bool Delete(int RoomId)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("SP_Room_Delete @RoomId", new object[] { RoomId });
+
+            return result > 0;
+        }
+
+
+        public List<Room> Search(string searchValue)
+        {
+            List<Room> list = new List<Room>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("SP_Room_Search @searchValue", new object[] { searchValue });
+            foreach (DataRow item in data.Rows)
+            {
+                Room entry = new Room(item);
+                list.Add(entry);
+            }
+            return list;
+        }
+    }
+}
